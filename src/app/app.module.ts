c3f60f6abf1dbf84bcd2fda7f13322c83734d4e6
Bruno Duarte Brito <bruno.brito@ideware.com.br>
2017-04-28 18:44:17 -0300

    Adding Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MaterializeModule } from 'ng2-materialize';
 
 import { HomeComponent } from './home/home.component';
+import { RootComponent } from './root/root.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // redirect first path
+  { path: '**', redirectTo: '/home', pathMatch: 'full' } // redirect invalid paths
+];
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    RootComponent
   ],
   imports: [
     HttpModule,
     FormsModule,
     BrowserModule,
-    MaterializeModule.forRoot()
+    MaterializeModule.forRoot(),
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [HomeComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
